03d651d5ccb36a9f8dd382c72063cd5bdaa12b9f
Eddie James <eajames@linux.ibm.com>
2022-09-19 16:06:48 -0500

    [binlog] Add patch to make tool build and install optional
    Signed-off-by: Eddie James <eajames@linux.ibm.com>

--- ports/binlog/portfile.cmake
+++ ports/binlog/portfile.cmake
@@ -6,10 +6,17 @@ vcpkg_from_github(
     REF 5467217310eb0c43452bbd810d3f093e6eb737d9 # 2021-04-16
     SHA512 d99c932eddee0109c8184714b05bd9f6397dff013828b287a76affa5dc2db8d5570b6e805697019bf59756f6a2d05a62e9456dd4b7d3f0831e7f8308be1ca733
     HEAD_REF master
+    PATCHES 0001-Make-bread-and-brecovery-optional.patch
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBINLOG_BUILD_BREAD=OFF
+        -DBINLOG_BUILD_BRECOVERY=OFF
+        -DBINLOG_BUILD_EXAMPLES=OFF
+        -DBINLOG_BUILD_UNIT_TESTS=OFF
+        -DBINLOG_BUILD_INTEGRATION_TESTS=OFF
 )
 
 vcpkg_cmake_install()
